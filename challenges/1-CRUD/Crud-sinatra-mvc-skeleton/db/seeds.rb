ce4899273be031b6039662ee11d80d7faadb6c3d
Leonardo Omaña Cortés <leonardoomanacortes@MacBook-Pro-de-Leonardo.local>
2017-11-11 21:01:09 -0500

    CRUD

--- challenges/1-CRUD/Crud-sinatra-mvc-skeleton/db/seeds.rb
+++
@@ -1,5 +0,0 @@
-require 'faker'
-
-10.times do |i|
-  Figther.create(name: Faker::DragonBall.name, age: rand(1) + 1)
-end
--- challenges/1-CRUD/crud-sinatra-mvc-skeleton/db/migrate/20171110182952_create_figthers.rb
+++
@@ -1,4 +0,0 @@
-class CreateFighters < ActiveRecord::Migration
-  def change
-  end
-end
